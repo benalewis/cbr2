39b15633adffb4da55d44d97fb580af52e0c7de4
Ben Lewis <benalewis@hotmail.co.uk>
2016-05-27 02:19:40 +0100

    First Change (Filling Data)
    The databases now have a healthy amount of data which can be changed at
    will.

--- MainActivity.java
+++ MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -78,7 +79,13 @@ public class MainActivity extends AppCompatActivity {
                 agiWe = Double.valueOf(agiWeight.getText().toString());
                 intWe = Double.valueOf(intelWeight.getText().toString());
 
-                createDatabase(); getAnswer();
+                   createDatabase();
+
+                    genHero(5, "Warrior");
+                    genHero(5, "Rogue");
+                    genHero(5, "Wizard");
+
+                    fillHeroList(); getAnswer();
                  }
 
                 catch (Exception e) {
@@ -113,9 +120,6 @@ public class MainActivity extends AppCompatActivity {
     public void createDatabase() {
         try {
 
-            double similarity = 0;
-            double resultSimilarity = 100;
-
             deleteDatabase("CBR");
 
             cbrDB = this.openOrCreateDatabase("CBR", MODE_PRIVATE, null);
@@ -123,54 +127,50 @@ public class MainActivity extends AppCompatActivity {
             cbrDB.execSQL("CREATE TABLE IF NOT EXISTS heroes " +
                     "(name VARCHAR, str DOUBLE(3), agi DOUBLE(3), int DOUBLE(3))");
 
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Warrior', 5.0, 0.0, 0.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Warrior', 4.5, 0.5, 0.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Warrior', 4.0, 0.0, 1.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Warrior', 3.0, 1.5, 0.5) ");
-
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Rogue', 0.5, 3.5, 1.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Rogue', 0.0, 5.0, 0.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Rogue', 0.5, 4.0, 0.5) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Rogue', 1.5, 3.0, 0.5) ");
-
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Wizard', 0.0, 0.0, 5.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Wizard', 1.0, 0.0, 4.0) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Wizard', 0.0, 0.5, 4.5) ");
-            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Wizard', 0.0, 1.5, 3.5) ");
-
-            Cursor c = cbrDB.rawQuery("SELECT * FROM heroes", null);
-
-            int nameIndex = c.getColumnIndex("name");
-            int strIndex = c.getColumnIndex("str");
-            int agiIndex = c.getColumnIndex("agi");
-            int intelIndex = c.getColumnIndex("int");
-
-            c.moveToFirst();
-
-            if (c.moveToFirst()) {
-
-                do {
-
-                    String name = c.getString(nameIndex);
-                    double str = c.getDouble(strIndex);
-                    double agi = c.getDouble(agiIndex);
-                    double intel = c.getDouble(intelIndex);
-
-                    //Log.i("Test: ", name + " " + String.valueOf(
-
-
-                    similarity = getSimilarity(str, targetStr, strWe,
-                            agi, targetAgi, agiWe,
-                            intel, targetIntel, intWe);
-
-                    heroList.add(new Hero(name, similarity));
-
-                } while (c.moveToNext());
-            }
+            //Test Data
+            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Warrior', 10.0, 0.0, 0.0) ");
+            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Rogue', 0.0, 10.0, 0.0) ");
+            cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Wizard', 0.0, 0.0, 10.0) ");
 
         } catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void fillHeroList() {
+
+        double similarity = 0;
+
+        Cursor c = cbrDB.rawQuery("SELECT * FROM heroes", null);
+
+        int nameIndex = c.getColumnIndex("name");
+        int strIndex = c.getColumnIndex("str");
+        int agiIndex = c.getColumnIndex("agi");
+        int intelIndex = c.getColumnIndex("int");
+
+        c.moveToFirst();
+
+        if (c.moveToFirst()) {
+
+            do {
+
+                String name = c.getString(nameIndex);
+                double str = c.getDouble(strIndex);
+                double agi = c.getDouble(agiIndex);
+                double intel = c.getDouble(intelIndex);
+
+                //Log.i("Test: ", name + " " + String.valueOf(
+
+                similarity = getSimilarity(str, targetStr, strWe,
+                        agi, targetAgi, agiWe,
+                        intel, targetIntel, intWe);
+
+                heroList.add(new Hero(name, similarity));
+
+            } while (c.moveToNext());
+        }
+
+        c.close();
     }
 
     public double getSimilarity( double str, double strT, double strW,
@@ -256,5 +256,61 @@ public class MainActivity extends AppCompatActivity {
         if (c > largest) { result = "Wizard"; }
 
         return result;
+    }
+
+    public void genHero (int entries, String hero) {
+
+        //gen a value between 3-5
+        //gen a value between 2-3
+        //last value = a - b;
+        //insert into database
+
+        int min = 4;
+        int max = 9;
+        Random r = new Random();
+
+        for (int i = 0; i < entries; i++) {
+
+            int i1 = r.nextInt(max - min + 1) + min;
+
+            int i2 = r.nextInt(10 - i1);
+
+            int i3 = r.nextInt(10 - i1 - i2);
+
+            i1 += (10 - (i1 + i2 + i3));
+
+            switch (hero) {
+                case "Warrior":
+
+                    //Warrior highest value Str, then Int, then Agi
+                    cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Warrior', " +
+                            String.valueOf(i1) + ", " +
+                            String.valueOf(i3) + ", " +
+                            String.valueOf(i2) + ") ");
+
+                    Log.i("Result: " + hero, i1 + ", " + i2 + ", " + i3);
+                    break;
+
+                case "Rogue":
+
+                    //Rogue highest value Agi, then Str, then Int
+                    cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Rogue', " +
+                            String.valueOf(i2) + ", " +
+                            String.valueOf(i1) + ", " +
+                            String.valueOf(i3) + ") ");
+                    Log.i("Result: " + hero, i2 + ", " + i1 + ", " + i3);
+                    break;
+
+                case "Wizard":
+
+                    //Wizard highest value Int, then Agi, then Str
+                    cbrDB.execSQL("INSERT INTO heroes (name, str, agi, int) VALUES ('Wizard', " +
+                            String.valueOf(i3) + ", " +
+                            String.valueOf(i2) + ", " +
+                            String.valueOf(i1) + ") ");
+                    Log.i("Result: " + hero, i3 + ", " + i2 + ", " + i1);
+                    break;
+            }
+        }
     }
 }
